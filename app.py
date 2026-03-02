20fe9e07c3cd59c34594a8b5f232e39ea316754d
Justin McCormick <karcadia@gmail.com>
2026-03-02 14:56:38 -0600

    Keep all the dict keys unique by prepending with sab_.

--- app.py
+++ app.py
@@ -533,12 +533,12 @@ def refresh_sabnzbd():
     url = f"http://nas.mccormicom.com:8081/api?apikey={SABNZBD_API_KEY}&output=json&mode=queue"
     resp = requests.request('GET', url=url)
     sab_queue = json.loads(resp.text)
-    sabnzbd['status']           = sab_queue['queue']['status']
-    sabnzbd['queue_size']       = sab_queue['queue']['noofslots']
-    sabnzbd['queue_space_left'] = sab_queue['queue']['sizeleft']
-    sabnzbd['queue_time_left']  = sab_queue['queue']['timeleft']
-    sabnzbd['free_space']       = sab_queue['queue']['diskspace1_norm']
-    sabnzbd['total_space']      = str(float(sab_queue['queue']['diskspacetotal1']) / 1000) + 'T'
+    sabnzbd['sab_status']           = sab_queue['queue']['status']
+    sabnzbd['sab_queue_size']       = sab_queue['queue']['noofslots']
+    sabnzbd['sab_queue_space_left'] = sab_queue['queue']['sizeleft']
+    sabnzbd['sab_queue_time_left']  = sab_queue['queue']['timeleft']
+    sabnzbd['sab_free_space']       = sab_queue['queue']['diskspace1_norm']
+    sabnzbd['sab_total_space']      = str(float(sab_queue['queue']['diskspacetotal1']) / 1000) + 'T'
 
 @app.route('/')
 def hello():
