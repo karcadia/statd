0c4fd2319e539ee2f797c89160436c84d339e6de
Justin McCormick <karcadia@gmail.com>
2026-03-06 11:38:44 -0600

    Round watts to the nearest 1 decimal.

--- app.py
+++ app.py
@@ -526,8 +526,8 @@ def refresh_emporia_data():
     dryer_usage_dict  = vue.get_device_list_usage(deviceGids=dryer.device_gid,  instant=None, scale=Scale.SECOND.value, unit=Unit.KWH.value)
     washer_usage_watt = washer_usage_dict[washer.device_gid].channels['1,2,3'].usage * 3600 * 1000
     dryer_usage_watt  = dryer_usage_dict[dryer.device_gid].channels['1,2,3'].usage * 3600 * 1000
-    emporia['Washer'] = str(round(washer_usage_watt, 3)) + 'W'
-    emporia['Dryer']  = str(round(dryer_usage_watt, 3)) + 'W'
+    emporia['Washer'] = str(round(washer_usage_watt, 1)) + 'W'
+    emporia['Dryer']  = str(round(dryer_usage_watt, 1)) + 'W'
 
 def refresh_sabnzbd():
     url = f"http://nas.mccormicom.com:8081/api?apikey={SABNZBD_API_KEY}&output=json&mode=queue"
